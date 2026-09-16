0e4f1ae586300c9a0b149bbae060d4eb39fe2a8c
misterD05 <mrD05.dev@protonmail.com>
2026-09-16 23:34:29 +0200

    little correction svg cloud

--- src/assets/meteo/cloud.tsx
+++ src/assets/meteo/cloud.tsx
@@ -12,6 +12,6 @@ export const AnimatedCloudIcon: React.FC<SvgIconProps> = ({
   className = ''
 }) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#7b93a8" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-cloud-icon lucide-cloud"><path d="M17.5 19H9a7 7 0 1 1 6.71-9h1.79a4.5 4.5 0 1 1 0 9Z"/></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox="0 0 24 24" fill="none" stroke="#7b93a8" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-cloud-icon lucide-cloud"><path d="M17.5 19H9a7 7 0 1 1 6.71-9h1.79a4.5 4.5 0 1 1 0 9Z"/></svg>
   );
 };
